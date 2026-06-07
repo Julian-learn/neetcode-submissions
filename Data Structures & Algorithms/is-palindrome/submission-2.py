0d2a518dbef8b1db23530c7a0997ef8f7e721907
Julian <147509461+Julian-learn@users.noreply.github.com>
2026-06-07 17:07:40 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        point1 = 0
+        point2 = len(s) - 1
+        while point1 < point2:
+            if not s[point1].isalnum():
+                point1 += 1
+                continue
+            if not s[point2].isalnum():
+                point2 -= 1
+                continue
+            
+            if not s[point1].lower() == s[point2].lower():
+                return False
+            point1 +=1
+            point2 -=1
+        return True
+                    
+
+        
