e89ed25d886bac440c38bf5ac1c555915f9def64
Julian <147509461+Julian-learn@users.noreply.github.com>
2026-07-05 09:51:22 +0200

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        #Didnt know about Deques yet, so i only managed to get O(n²) time complexity solution
+        #revisiting later
+        res = []
+        hashmap = {}
+        cur = float("-infinity")
+        r = 0
+
+        while k > r:
+            hashmap[nums[r]] = hashmap.get(nums[r], 0) + 1
+            cur = max(nums[r], cur)
+            r += 1 
+        
+        res.append(cur)
+        length = len(nums)
+        l = 0
+        while r < length:
+            if hashmap[nums[l]] <= 1:
+                del hashmap[nums[l]]
+            else:
+                hashmap[nums[l]] -= 1
+            l += 1
+            hashmap[nums[r]] = hashmap.get(nums[r], 0) + 1
+            
+            cur = max(hashmap.keys())
+            r += 1
+            res.append(cur)
+        return res
+        
+
+
+
+
+
+
+
+
+        
