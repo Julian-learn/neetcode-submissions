f39e85a067672a31c29cb8aa50df6bf02207cdcd
Julian <147509461+Julian-learn@users.noreply.github.com>
2026-08-04 22:41:06 +0200

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def recursion(op, cl):
+            if op == cl == n:
+                res.append("".join(stack))
+                return
+
+            if op < n:
+                stack.append("(")
+                recursion(op + 1, cl)
+                stack.pop()
+            if cl < op:
+                stack.append(")")
+                recursion(op, cl + 1)
+                stack.pop()
+
+
+
+
+        recursion(0, 0)
+        return res
